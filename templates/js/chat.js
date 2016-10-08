a11b2441da5c3862f569d02782f054650c35a0c8
Fraser Price <fraser@Frasers-MacBook-Air.local>
2016-10-08 16:20:55 +0200

    JS shit xdddd

--- templates/js/chat.js
+++ templates/js/chat.js
@@ -1,3 +1,5 @@
+var socket;
+
 $(document).ready(function() {
 
     $("#btn-chat").click({author: "asdf", msg: $("#btn-input")}, sendMessage);
@@ -8,6 +10,8 @@ $(document).ready(function() {
 });
 
 function sendMessage(event) {
+    socket.emit('message_submitted', { message: "TODO"});
+
     var post='<li class="left clearfix"><span class="chat-img pull-left"> <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" /> </span> <div class="chat-body clearfix"> <div class="header"> <strong class="primary-font">' + event.data.author + '</strong> <small class="pull-right text-muted"> <span class="glyphicon glyphicon-time"></span>12 mins ago</small> </div> <p>' + event.data.msg + '</p> </div> </li>'
 
     $(".chat").append(post);
@@ -19,20 +23,28 @@ function initChat(event) {
   $('#btn-chat').prop('disabled', false);
   $('#btn-input').prop('disabled', false);
 
-  //colorize
   $("#chat-panel").css("background-color", "#337AB7");
   $("#chat-panel").css("border-bottom", "#337AB7");
   $(".panel").css("border-color", "#337AB7");
 
   $("html, body").animate({ scrollTop: $('.chat').offset().top }, 500);
   
-  var socket = new WebSocket('ws://10.192.216.241:5000');
-  socket.send("xd");
-  socket.onopen = function() {
-    alert("connected");
-     socket.send("h a r a m b e w a s i n n o c e n t");
-  }
-  socket.onmessage = function(event) {
-    alert("eeeeeeyyyyy");
-  }
+  socket = io('http://localhost:5000');
+  socket.emit("start_request", { nickname: "TODO" });
+
+  socket.on('started', function(data) {
+    var role = "TODO";
+  });
+
+  socket.on('message_request', function(data) {
+    var timeout = "TODO";
+  });
+
+  socket.on('message_received', function(data) {
+    $(".chat").append("TODO");
+  });
+
+  socket.on('finished', function(data) {
+    $(".chat").append("TODO");
+  });
 }
