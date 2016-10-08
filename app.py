f5902896111d7faed3b2575df5e48b5df680d733
Virain Gupta <virain.gupta15@imperial.ac.uk>
2016-10-08 15:18:50 +0100

    Interface template in app.py.

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 
-
 def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
@@ -23,87 +22,45 @@ def background_thread():
                       {'data': 'Server generated event', 'count': count},
                       namespace='/test')
 
-
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
+@socketio.on('start request', namespace='/chat')
+def start_request(message):
+    print 'start request'
+    # TODO: start matchmaking process
+    # global thread
+    # if thread is None:
+    #     thread = socketio.start_background_task(target=background_thread)
+    print message['nickname']
 
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+@socketio.on('message submitted', namespace='/chat')
+def message_submitted(message):
+    print 'message submitted'
+    # TODO: notify game
+    print message['message']
 
+@socketio.on('bot decision', namespace='/chat')
+def bot_decision(message):
+    print 'bot decision'
+    # TODO: notify game
+    print message['bot']
 
-@socketio.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('close_room', namespace='/test')
-def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-
-@socketio.on('my_room_event', namespace='/test')
-def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
-
-
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
+@socketio.on('stop_request', namespace='/chat')
+def stop_request():
+    print 'stop request'
+    # TODO: norufy gane
     disconnect()
 
-
-@socketio.on('my_ping', namespace='/test')
-def ping_pong():
-    emit('my_pong')
-
-
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/chat')
 def test_connect():
-    global thread
-    if thread is None:
-        thread = socketio.start_background_task(target=background_thread)
-    emit('my_response', {'data': 'Connected', 'count': 0})
+    # TODO: 
+    print 'connect'
 
-
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect', namespace='/chat')
 def test_disconnect():
-    print('Client disconnected', request.sid)
-
+    print 'disconnect'
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0')
