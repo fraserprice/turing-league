9db5f78319a54a088c43843aac312ecb1a453968
Virain Gupta <virain.gupta15@imperial.ac.uk>
2016-10-08 22:28:52 +0100

    Matchmaking progress.

--- bots/bot.py
+++ bots/bot.py
@@ -1,8 +1,10 @@
 from chatterbotapi import ChatterBotFactory, ChatterBotType
+
 class Bot:
 
     def __init__(self, bot_type):
         factory = ChatterBotFactory()
+        self._bot_type = bot_type
         if (bot_type == "cleverbot"):
             self.bot = factory.create(ChatterBotType.CLEVERBOT)
         elif (bot_type == "pandorabots"):
@@ -12,5 +14,11 @@ class Bot:
         else:
             print("Invalid bot type")
 
+    def bot_type(self):
+        return self._bot_type
+
     def start_session(self, bot):
         return self.bot.create_session()
+
+BOTS = [Bot("cleverbot"), Bot("pandorabots")]
+
